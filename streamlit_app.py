c0ec436e604797976f98fd6a51a6563e878ad2e0
Kyar Phae <107622233+lawrencenuu@users.noreply.github.com>
2025-06-25 11:13:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 # Import python packages
-python -m pip install --upgrade pip
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
